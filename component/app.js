4173d0b153ad49a4e6f6bca6d095698d769687c4
Sergio Vilar <sergiovilar.r@gmail.com>
2015-10-25 23:39:02 -0300

    Moved app logic to Main component
    This change makes easy create other components

--- component/app.js
+++ component/app.js
@@ -1,12 +1,7 @@
-var React = require('react'),
-ReactDOM = require('react-dom'),
-Main = require('./main/Main.js'),
-SeriesList = require('./main/SeriesList.js');
-var remote = window.require('remote');
-window.React = React;
+var React = require('react')
+  , ReactDOM = require('react-dom')
+  , Main = require('./main/Main.js');
 
 ReactDOM.render(
-  <div className="window-content">
-    <SeriesList />
-  </div>
-  , document.querySelector('.window'));
+  <Main />
+, document.querySelector('.window'));
--- component/main/SeriesList.js
+++ component/main/SeriesList.js
@@ -1,53 +1,18 @@
 import React from 'react'
-var remote = window.require('remote'),
-SubList = require('./SubList.js');
 
 var SeriesList = React.createClass({
+
   displayName: 'SeriesList',
-  getInitialState: function() {
-    return {
-      loading: true,
-      subtitles: []
-    };
-  },
-  getSubs: function(index, callback) {
-    remote.require('./src/index.js')(index, function(found) {
-      callback(found);
-    });
-  },
-  componentDidMount: function() {
-    var that = this;
-    that.getSubs(0, function(subs){
-      if(that.isMounted())
-        that.setState({
-          subtitles: subs,
-          loading: false
-        });
-    });
-  },
-  getSeries: function() {
-      return remote.require('./data.json').series;
-  },
+
   handleClick: function(i) {
-    var that = this;
-    that.setState({
-      loading: true,
-      subtitles: []
-    });
-    that.getSubs(i, function(subs){
-      that.setState({
-        subtitles: subs,
-        loading: false
-      });
-    });
+    this.props.callback(i);
   },
   render: function() {
     return (
-      <div className="pane-group">
         <div className="pane pane-sm sidebar">
           <nav className="nav-group">
             <h5 className="nav-group-title">Series</h5>
-            {this.getSeries().map(function(item, i) {
+            {this.props.getData().map(function(item, i) {
               return (<a onClick={this.handleClick.bind(this, i)} key={i}>
                 <span className="nav-group-item">
                 {item.name}
@@ -55,8 +20,6 @@ var SeriesList = React.createClass({
             }, this)}
           </nav>
         </div>
-        <SubList items={this.state.subtitles} loading={this.state.loading} />
-      </div>
     );
   }
 });
