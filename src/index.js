cf556623a30a7d478bac107b9bb5ecc54e91981f
Sergio Vilar <sergiovilar.r@gmail.com>
2016-03-07 23:33:55 -0300

    Converted tabs to spaces

--- src/index.js
+++ src/index.js
@@ -10,22 +10,22 @@ require('crash-reporter').start();
 let mainWindow = null;
 
 app.on('window-all-closed', () => {
-	if (process.platform !== 'darwin') app.quit();
+  if (process.platform !== 'darwin') app.quit();
 });
 
 app.on('ready', () => {
 
-	mainWindow = new BrowserWindow({
-		width: 1020,
-		height: 640,
-		'min-width': 1020,
-		'min-height': 640,
-		frame: true
-	});
+  mainWindow = new BrowserWindow({
+    width: 1020,
+    height: 640,
+    'min-width': 1020,
+    'min-height': 640,
+    frame: true
+  });
 
-	mainWindow.loadUrl('file://' + __dirname + '/index.html');
-	mainWindow.on('closed', () => {
-		mainWindow = null;
-	});
+  mainWindow.loadUrl('file://' + __dirname + '/index.html');
+  mainWindow.on('closed', () => {
+    mainWindow = null;
+  });
 
 });
