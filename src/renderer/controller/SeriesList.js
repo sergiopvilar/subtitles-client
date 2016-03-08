9c4542e3fa322f88ef93ab09bf7b144ff8a2f1b7
Sergio Vilar <sergiovilar.r@gmail.com>
2016-03-08 00:16:21 -0300

    Show active TV Show on sidebar

--- src/renderer/controller/SeriesList.js
+++ src/renderer/controller/SeriesList.js
@@ -3,8 +3,10 @@ const Series = remote.require('./browser/model/Serie.js');
 app.controller('SeriesListController', ['$scope', '$rootScope', '$location', ($scope, $rootScope, $location) => {
 
   $scope.series = Series.chain().value();
+  $scope.selected = '';
 
   $scope.load = (id) => {
+    $scope.selected = id;
     $location.path( "/subtitles/"+id );
   };
 
